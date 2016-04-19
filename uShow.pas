5728275bce353b328308db66402fc26065b156ce
FNicon <mybeddrom@gmail.com>
2016-04-19 21:52:34 +0700

    Added files via upload

---
+++ uShow.pas
@@ -0,0 +1,33 @@
+unit ushow;
+
+interface
+	uses uFilm;
+
+	procedure showMovie (mainF: Film);
+	{I.S : Judul terdefinisi}
+	{F.S : Menampilkan properties lain dari film}
+
+implementation
+	procedure showMovie (mainF: Film);												{data from database_film.txt}
+	{KAMUS}
+	var
+		i : integer;																{counter}
+		judul : string;																{input judul}
+
+	begin
+		write('Masukkan judul : ');
+		readln(judul);																{input}
+		for i:=1 to mainF.size do													{cek hingga batas array}
+		begin
+			if judul=mainF.contents[i].judul then									{Bila nemu judul, tampilkan yang lain}
+			begin
+				writeln('Genre 		    	: ',mainF.contents[i].genre);			{output}
+				writeln('ViewerRating 		: ',mainF.contents[i].viewer);			{output}
+				writeln('Durasi		    	: ',mainF.contents[i].durasi);			{output}
+				writeln('Sinopsis		    : ',mainF.contents[i].sinopsis);		{output}
+				writeln('HargaWeekdays		: ',mainF.contents[i].pWeekdays);		{output}
+				writeln('HargaWeekdend		: ',mainF.contents[i].pWeekend);		{output}
+			end;
+		end;
+	end;
+end.
