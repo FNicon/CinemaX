9f7c7abb845558c7333cb60b9634112e9e435685
chessanur <chessanur@gmail.com>
2016-04-21 23:18:43 +0700

    Update uNowPlaying.pas

--- uNowPlaying.pas
+++ uNowPlaying.pas
@@ -3,14 +3,15 @@ unit uNowPlaying;
 interface
 	uses uConfig, uFilm, uLoader, uParser;
 	procedure nowPlaying (mainf:Film);
+	{I.S : mainf bertipe film sudah terdefinisi}
+	{F.S : Menampilkan semua film pada hari ini}
 
 implementation
-procedure nowPlaying (mainf: Film); {menampilkan film apa saja yang ditayangkan hari ini}
+procedure nowPlaying (mainf: Film); 
 var i : integer;
 begin
 	for i:=1 to mainf.size do
 	begin
-		write ('>');
 		writeln( mainf.contents[i].judul); {menghasilkan output judul film yang ditayangkan hari ini}
 	end;
 end;
