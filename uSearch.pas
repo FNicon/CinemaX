bc6602a01be1dce5e23ed204a3eeb6b531b4df1e
fildfreeze <fildahananda@gmail.com>
2016-04-22 05:38:10 +0700

    Update uSearch.pas

--- uSearch.pas
+++ uSearch.pas
@@ -1,86 +1,69 @@
 unit uSearch;
 
+
+
 interface
 	uses uFilm;
-
+	
 	var
 	Movie : Film;
+
+	procedure searchMovie();
 	
-	procedure searchMovie(
-	);
 
 implementation
 
-	procedure searchMovie(
-	);
+
+	procedure searchMovie();
+
 	var
-	keyword : string;
-	i, j, k, l, idk: integer;
-	
-	begin
+		keyword : string;
+		i, j, k, idk, l, tulis : integer;
+		Cek : boolean;
+
+	begin		
+		tulis:=0;
 		write('Input keyword: '); readln(keyword);
+
 		
-		for j:=1 to Movie.size do {akses ke tiap element di array}
+		For j:=1 to Movie.size do 
 		begin
-			for k:=1 to length(Movie.contents[j].judul) do {akses ke judul}
+		Cek := False;
+		k:=1;
+		
+			while (k<length(Movie.contents[j].judul)) and Cek=False do {akses ke tiap element di array}
 			begin
-				if keyword[1]=Movie.contents[j].judul[k] then {jika huruf pertama keyword sama dengan sebuah huruf di judul}
-					begin
-						{cek untuk huruf pada keyword selanjutnya, apakah sama dengan huruf selanjutnya di judul}
-						idk:=1;
-						i:=1;
-						l:=k;
-						while (keyword[i]=Movie.contents[j].judul[l]) and (i<=length(keyword)) do
+			if keyword[1]=Movie.contents[j].judul[k] then {jika huruf pertama keyword sama dengan sebuah huruf di judul}
+				begin
+					{cek untuk huruf pada keyword selanjutnya, apakah sama dengan huruf selanjutnya di judul}
+					idk:=1;
+					i:=1;
+					l:=k;
+					while (keyword[i]=Movie.contents[j].judul[l]) and (i<length(keyword)) do
 						begin
 							idk:=idk+1;
 							i:=i+1;
 							l:=l+1;								
 						end;
 						
-						if idk=length(keyword) then {jika keyword ditemukan di judul}
-							writeln(Movie.contents[j].judul);
-					end;
-			end;
-			
-			for k:=1 to length(Movie.contents[j].genre) do
-			begin
-				if keyword[1]=Movie.contents[j].genre[k] then
-					begin
-						idk:=1;
-						i:=1;
-						l:=k;
-						while (keyword[i]=Movie.contents[j].genre[l]) and (i<=length(keyword)) do
+					if idk=length(keyword) then {jika keyword ditemukan di judul}
 						begin
-							idk:=idk+1;
-							i:=i+1;
-							l:=l+1;								
-						end;
-						
-						if idk=length(keyword) then
 							writeln(Movie.contents[j].judul);
-					end;
-			end;
-			
-			for k:=1 to length(Movie.contents[j].sinopsis) do
-			begin
-				if keyword[1]=Movie.contents[j].sinopsis[k] then
-					begin
-						idk:=1;
-						i:=1;
-						l:=k;
-						while (keyword[i]=Movie.contents[j].sinopsis[l]) and (i<=length(keyword)) do
+							Cek:=True;
+							tulis:=tulis+1;
+						end
+					else
 						begin
-							idk:=idk+1;
-							i:=i+1;
-							l:=l+1;								
+							k:=k+1;
 						end;
-						
-						if idk=length(keyword) then
-							writeln(Movie.contents[j].judul);
-					end;
+				end else
+							k:=k+1;
 			end;
+		
 			
 		end;
+		
+		if tulis=0 then {Jika tidak ditemukan keyword]
+		writeln('Keyword not found');
 	end;
-
 end.
