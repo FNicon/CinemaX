f37701fb7255abc6ee743687559b765b4a928a9f
FNicon <mybeddrom@gmail.com>
2016-04-21 20:30:57 +0700

    Update uShow.pas

--- uShow.pas
+++ uShow.pas
@@ -8,23 +8,23 @@ interface
 	{F.S : Menampilkan properties lain dari film}
 
 implementation
-	procedure showMovie (mainF: Film);												{data from database_film.txt}
+	procedure showMovie (mainF: Film);									{data from database_film.txt}
 	{KAMUS}
 	var
-		i : integer;																{counter}
-		judul : string;																{input judul}
+		i : integer;											{counter}
+		judul : string;											{input judul}
 
 	begin
 		write('Masukkan judul : ');
-		readln(judul);																{input}
-		for i:=1 to mainF.size do													{cek hingga batas array}
+		readln(judul);											{input}
+		for i:=1 to mainF.size do									{cek hingga batas array}
 		begin
-			if judul=mainF.contents[i].judul then									{Bila nemu judul, tampilkan yang lain}
+			if judul=mainF.contents[i].judul then							{Bila nemu judul, tampilkan yang lain}
 			begin
 				writeln('Genre 		    	: ',mainF.contents[i].genre);			{output}
 				writeln('ViewerRating 		: ',mainF.contents[i].viewer);			{output}
 				writeln('Durasi		    	: ',mainF.contents[i].durasi);			{output}
-				writeln('Sinopsis		    : ',mainF.contents[i].sinopsis);		{output}
+				writeln('Sinopsis		: ',mainF.contents[i].sinopsis);		{output}
 				writeln('HargaWeekdays		: ',mainF.contents[i].pWeekdays);		{output}
 				writeln('HargaWeekdend		: ',mainF.contents[i].pWeekend);		{output}
 			end;
