72102744d9965fc5e12338c425d3afee39a0fce8
FNicon <mybeddrom@gmail.com>
2016-04-23 19:53:02 +0700

    Update uPriceX.pas

--- uPriceX.pas
+++ uPriceX.pas
@@ -1,68 +1,49 @@
 unit uPriceX;
 
 interface
-{pW1 dan pW2 adalah harga weekdays dan weekend}
-var
-seat : integer;
-hari : string;
-pW1, pW2 : longint;
+	{pW1 dan pW2 adalah harga weekdays dan weekend}
+	var
+		seat : integer;
+		hari : string;
+		pW1, pW2 : longint;
 
 function hargaCC (seat : integer; hari : string; pW1, pW2 : longint) : longint;
-
 function hargaM (seat : integer; hari : string; pW1, pW2 : longint) : longint;
 
-
-
-
 implementation
-
 
 function hargaCC (seat : integer; hari : string; pW1, pW2 : longint) : longint;
 	begin
 		if (hari='Sabtu') then
+		begin
+                	if(seat >= 2) then
+                	begin
+                   		hargaCC := pW2 * (seat - 1);
+                    	end 
+                    	else {seat=1}
+                    	begin
+                         	hargaCC := pW2 * seat
+                    	end;
+		end 
+		else if (hari='Minggu') then
 			begin
-                    if(seat >= 2) then
-                    begin
-                   	 hargaCC := pW2 * (seat - 1);
-                    end else {seat=1}
-                    begin
-                         hargaCC := pW2 * seat
-                    end;
-			end else
-		if (hari='Minggu') then
-			begin
-			hargaCC := pW2 * seat;
-			end else
-        {Senin/Selasa/Rabu/Kamis/Jumat}
-			begin
+				hargaCC := pW2 * seat;
+		end 
+		else	{Senin/Selasa/Rabu/Kamis/Jumat}
+		begin
 			hargaCC := pW1 * seat;
-			end;
+		end;
 	end;
 
 function hargaM (seat : integer; hari : string; pW1, pW2 : longint) : longint;
-    begin
-        if ((hari='Sabtu') or (hari='Minggu')) then
-			begin
+    	begin
+        	if ((hari='Sabtu') or (hari='Minggu')) then
+		begin
 			hargaM := pW2 * seat;
-			end else
-        {Senin/Selasa/Rabu/Kamis/Jumat}
-			begin
+		end 
+		else	{Senin/Selasa/Rabu/Kamis/Jumat}
+		begin
 			hargaM := pW1 * seat;
-			end;
-    end;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		end;
+    	end;
 end.
