8ce97cf2cc04b653856224cdfb10322e8c1ac494
umardragneel <umardragneel@gmail.com>
2016-04-23 16:06:53 +0700

    Added files via upload

---
+++ uStart.pas
@@ -0,0 +1,29 @@
+unit uStart;
+interface
+
+procedure start();
+
+implementation
+procedure start();
+begin
+    writeln('   _____   _                                       __   __');
+    writeln('  / ____| (_)                                      \ \ / /  _  __  ____  ');
+    writeln(' | |       _   _ __     ___   _ __ ___     __ _     \ v /  / |/ /_| ___| ');
+    writeln(' | |      | | | `_ \   / _ \ | `_ ` _ \   / _` |     > <   | | `_ \___ \');
+    writeln(' | |____  | | | | | | |  __/ | | | | | | | (_| |    / . \  | | (_) |__) |');
+    writeln('  \_____| |_| |_| |_|  \___| |_| |_| |_|  \__,_|   /_/ \_\ |_|\___/____/');
+    writeln;
+    writeln('                                                      ver 1.0.1');
+    writeln;
+    writeln;
+    writeln;
+    writeln;
+    writeln('                     16515044 Ray Andrew Obaja Sinurat');
+    writeln('                     16515072 Finiko Kasula Novenda');
+    writeln('                     16515289 Fildah Ananda Amalia');
+    writeln('                     16515324 Chessa Nur Triejunita');
+    writeln('                     16515345 Umar Al Faruqi');
+    readln;
+end;
+
+end.
